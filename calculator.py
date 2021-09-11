6671706e0f77190ba79f89862efae8283e09aac5
Samson Eromonsei <eromonsei.o.samson@gmail.com>
2021-09-11 17:38:48 -0500

    repo update

--- calculator.py
+++ calculator.py
@@ -3,13 +3,13 @@ Calculator library containing baisic math operations
 """
 
 
-def add(first_term, second_term):
+def add(first_term: int, second_term: int) -> int:
     return first_term + second_term
 
 
-def subtract(first_term, second_term):
+def subtract(first_term: int, second_term: int) -> int:
     return first_term - second_term
 
 
-def multiply(first_term, second_term):
+def multiply(first_term: int, second_term: int) -> int:
     return first_term * second_term
