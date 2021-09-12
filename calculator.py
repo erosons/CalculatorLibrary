5b0e866a588c70f0209d5f5ffca328c5fb8f2b5b
Samson Eromonsei <eromonsei.o.samson@gmail.com>
2021-09-11 19:31:50 -0500

    case experimented

--- calculator.py
+++ calculator.py
@@ -3,6 +3,9 @@ Calculator library containing baisic math operations
 """
 
 
+from typing import List
+
+
 def add(first_term: int, second_term: int) -> int:
     return first_term + second_term
 
@@ -13,3 +16,13 @@ def subtract(first_term: int, second_term: int) -> int:
 
 def multiply(first_term: int, second_term: int) -> int:
     return first_term * second_term
+
+
+def joiner(xs: List[int], delimiter: str) -> str:
+    generated_string: str = ""
+    for item in xs:
+        if generated_string == "":
+            generated_string = str(item)
+        else:
+            generated_string += delimiter + str(item)
+    return generated_string
--- test_calculator.py
+++ test_calculator.py
@@ -7,11 +7,25 @@ import calculator
 
 
 class TestCalculator:
-    def test_addition(self):
+    def test_addition(self) -> None:
         assert 4 == calculator.add(2, 2)
 
-    def test_subraction(self):
+    def test_subraction(self) -> None:
         assert 3 == calculator.subtract(5, 2)
 
-    def test_nuliplication(self):
+    def test_nuliplication(self) -> None:
         assert calculator.multiply(10, 10) == 100
+
+    """ Testing possible use case and edge cases a join funcion"""
+
+    def test_join_case1(self) -> None:
+        assert calculator.joiner([1, 2, 3], ",") == "1,2,3"
+
+    def test_join_use_case2(self) -> None:
+        assert calculator.joiner([], ",") == ""
+
+    def test_join_edge_case(self) -> None:
+        assert calculator.joiner([1, 2, 3], "") == "123"
+
+    def test_join_edge_case2(self) -> None:
+        assert calculator.joiner([1], ",") == "1"
